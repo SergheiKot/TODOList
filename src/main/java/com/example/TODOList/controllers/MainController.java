02200c8e805e30170d6efa9cc294bb4f3ae4f699
SergheiKot <sergheikotwork@gmail.com>
2019-10-21 20:28:51 +0300

    Добавил набросок контроллера

--- src/main/java/com/example/TODOList/controllers/MainController.java
+++ src/main/java/com/example/TODOList/controllers/MainController.java
@@ -1,4 +1,35 @@
 package com.example.TODOList.controllers;
 
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api")
 public class MainController {
+
+    @GetMapping
+    public String list() {
+        return "Test";
+    }
+
+    @GetMapping("{id}")
+    public String getOne(@PathVariable String id) {
+        JSONObject answer = new JSONObject().append(id, "Task");
+        return answer.toString();
+    }
+
+    @PostMapping
+    public String create(@RequestBody String task) {
+        return "Post: " + task;
+    }
+
+    @PutMapping("{id}")
+    public String update(@PathVariable String id, @RequestBody String task) {
+        return "Put: " + id + "\n" + task;
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id) {
+
+    }
 }
