4d2cfc7e8774e3373b860d17a1ccb02d0f77da07
SergheiKot <sergheikotwork@gmail.com>
2019-10-22 15:35:10 +0300

    Добавил логику в ApiController

--- src/main/java/com/example/TODOList/controllers/ApiController.java
+++ src/main/java/com/example/TODOList/controllers/ApiController.java
@@ -1,35 +1,73 @@
 package com.example.TODOList.controllers;
 
+import com.example.TODOList.hibernate.entities.Task;
+import com.example.TODOList.services.TaskService;
+import javassist.NotFoundException;
 import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api")
 public class ApiController {
 
+    private TaskService taskService = new TaskService();
+
     @GetMapping
-    public String getAll() {
-        return "Test";
+    public ResponseEntity getAll() {
+        List<Task> taskList = taskService.findAllTasks();
+        try {
+            JSONObject answer = new JSONObject();
+            taskList.forEach(x -> {
+                answer.append(Integer.toString(x.getId()), x.getTask());
+            });
+            return ResponseEntity.ok(answer.toString());
+        } catch (NullPointerException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
 
     @GetMapping("{id}")
-    public String getOne(@PathVariable String id) {
-        JSONObject answer = new JSONObject().append(id, "Task");
-        return answer.toString();
+    public ResponseEntity getOne(@PathVariable int id) throws NotFoundException {
+
+        Task task = taskService.findTask(id);
+        try {
+            JSONObject answer = new JSONObject().append("" + id, task.getTask());
+            return ResponseEntity.ok(answer.toString());
+        } catch (NullPointerException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
 
     @PostMapping
-    public String create(@RequestBody String task) {
-        return "Post: " + task;
+    public void create(@RequestBody String task) {
+        Task dbTask = new Task(task);
+        taskService.saveTask(dbTask);
     }
 
     @PutMapping("{id}")
-    public String update(@PathVariable String id, @RequestBody String task) {
-        return "Put: " + id + "\n" + task;
+    public ResponseEntity update(@PathVariable int id, @RequestBody String task) {
+        Task dbTask = taskService.findTask(id);
+
+        if (dbTask == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        dbTask.setTask(task);
+        taskService.updateTask(dbTask);
+        return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 
     @DeleteMapping("{id}")
-    public void delete(@PathVariable String id) {
+    public ResponseEntity delete(@PathVariable int id) {
+        Task dbTask = taskService.findTask(id);
 
+        if (dbTask == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        taskService.deleteTask(dbTask);
+        return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 }
