0a0dc199731690231263301141168ef8f2fdeb3b
SergheiKot <sergheikotwork@gmail.com>
2019-10-22 14:24:49 +0300

    Добавил сущность для БД

---
+++ src/main/java/com/example/TODOList/hibernate/entities/Task.java
@@ -0,0 +1,31 @@
+package com.example.TODOList.hibernate.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "task")
+    private String task;
+
+    public Task() {}
+
+    public Task(String task) {
+        this.task = task;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+}
